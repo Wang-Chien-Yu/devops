0855fe3e19e50f250cc2bbaf5b257856e2443b9b
josh <josh@joshdeMacBook-Pro.local>
2025-12-14 18:46:25 +0800

    test: test script

--- jenkins-libs/docker.groovy
+++ jenkins-libs/docker.groovy
@@ -1,5 +1,53 @@
-def dockerLogin(registry, Map args = [:]) {
-  def credentialsId = args.credentialsId
+/**
+ * Jenkins Shared Library
+ * Usage:
+ *   docker(
+ *     action: 'login',
+ *     registry: 'harbor.xxx.com',
+ *     credentialsId: 'harbor-admin'
+ *   )
+ *
+ *   docker(
+ *     action: 'build',
+ *     image: 'harbor.xxx.com/demo/app:tag'
+ *   )
+ *
+ *   docker(
+ *     action: 'push',
+ *     image: 'harbor.xxx.com/demo/app:tag'
+ *   )
+ */
+def call(Map args = [:]) {
+  def action = args.action
+
+  if (!action) {
+    error "docker: action is required (login | build | push)"
+  }
+
+  switch (action) {
+    case 'login':
+      dockerLogin(args)
+      break
+    case 'build':
+      dockerBuild(args)
+      break
+    case 'push':
+      dockerPush(args)
+      break
+    default:
+      error "docker: unsupported action '${action}'"
+  }
+}
+
+/* ===================== internal methods ===================== */
+
+private def dockerLogin(Map args) {
+  def registry       = args.registry
+  def credentialsId  = args.credentialsId
+
+  if (!registry || !credentialsId) {
+    error "docker login requires registry and credentialsId"
+  }
 
   withCredentials([
     usernamePassword(
@@ -15,12 +63,20 @@ def dockerLogin(registry, Map args = [:]) {
   }
 }
 
-def build(image) {
+private def dockerBuild(Map args) {
+  def image = args.image
+  if (!image) {
+    error "docker build requires image"
+  }
+
   sh "docker build -t ${image} ."
 }
 
-def push(image) {
+private def dockerPush(Map args) {
+  def image = args.image
+  if (!image) {
+    error "docker push requires image"
+  }
+
   sh "docker push ${image}"
 }
-
-return this
