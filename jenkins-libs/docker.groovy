57b8c56b61001adf15e6ba584ff8d9f37d64f251
josh <josh@joshdeMacBook-Pro.local>
2025-12-14 18:02:28 +0800

    test: test-jenkinsfile

--- jenkins-libs/docker.groovy
+++ jenkins-libs/docker.groovy
@@ -1,27 +1,27 @@
-def dockerLogin(registry, Map args = [:]) {
-    def script = this
+// 注意：移除了 def script = this，并添加了 script 参数
+def dockerLogin(script, registry, Map args = [:]) {
   def credentialsId = args.credentialsId
 
-  withCredentials([
-    usernamePassword(
+  script.withCredentials([ // 必须使用 script.withCredentials
+    script.usernamePassword(
       credentialsId: credentialsId,
       usernameVariable: 'DOCKER_USER',
       passwordVariable: 'DOCKER_PASS'
     )
   ]) {
-    sh """
+    script.sh """ // 必须使用 script.sh
       echo "\$DOCKER_PASS" | docker login ${registry} \
         -u "\$DOCKER_USER" --password-stdin
     """
   }
 }
 
-def dockerBuild(image) {
-  sh "docker build -t ${image} ."
+def dockerBuild(script, image) { // 添加 script 参数
+  script.sh "docker build -t ${image} ." // 必须使用 script.sh
 }
 
-def dockerPush(image) {
-  sh "docker push ${image}"
+def dockerPush(script, image) { // 添加 script 参数
+  script.sh "docker push ${image}" // 必须使用 script.sh
 }
 
 return this
