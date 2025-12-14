5bc5a25a831649c4848bf4c2980cf5b049d95abb
josh <josh@joshdeMacBook-Pro.local>
2025-12-14 18:19:43 +0800

    test: test-jenkinsfile

--- jenkins-libs/docker.groovy
+++ jenkins-libs/docker.groovy
@@ -1,27 +1,25 @@
-def dockerLogin(registry) {
-    echo "ℹ️  testtttttt"
-//   def credentialsId = args.credentialsId
+def dockerLogin(registry, Map args = [:]) {
+  def credentialsId = args.credentialsId
 
-//   withCredentials([
-//     usernamePassword(
-//       credentialsId: credentialsId,
-//       usernameVariable: 'DOCKER_USER',
-//       passwordVariable: 'DOCKER_PASS'
-//     )
-//   ]) 
-//   {
-//     sh """
-//       echo "\$DOCKER_PASS" | docker login ${registry} \
-//         -u "\$DOCKER_USER" --password-stdin
-//     """
-//   }
+  withCredentials([
+    usernamePassword(
+      credentialsId: credentialsId,
+      usernameVariable: 'DOCKER_USER',
+      passwordVariable: 'DOCKER_PASS'
+    )
+  ]) {
+    sh """
+      echo "\$DOCKER_PASS" | docker login ${registry} \
+        -u "\$DOCKER_USER" --password-stdin
+    """
+  }
 }
 
-def dockerBuild(image) {
+def build(image) {
   sh "docker build -t ${image} ."
 }
 
-def dockerPush(image) {
+def push(image) {
   sh "docker push ${image}"
 }
 
